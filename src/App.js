68e695f30e22829def490b2b256ffd7d56f3a8a9
boris <boris0815@naver.com>
2021-03-26 13:58:19 +0900

    master
    로고 삭제 및 팀명 팀원 display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    
-      </header>
       <p>
         17팀
         김현국
@@ -14,8 +12,10 @@ function App() {
         전준형
         이승우
         권철
-        
+
       </p>
+      </header>
+      
     </div>
   );
 }
