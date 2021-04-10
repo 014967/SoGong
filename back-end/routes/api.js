94b7f994a0790f925bfcc290760f2acae5175e12
Jun <rozo1136@gmail.com>
2021-04-11 02:24:17 +0900

    feat: upload image

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -1,80 +1,23 @@
 const express = require('express');
-const { User } = require('../models/users');
+const User = require('../models/users');
 const Event = require('../models/events');
 const Product = require('../models/products');
-const { auth } = require('../middleware/auth');
 const router = express.Router();
 const path = require("path");
 
-//role 1 관리자
-//role 0 일반유저
-router.get("/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        id: req.user.id,
-        email: req.user.email,
-        name: req.user.name,
-        role: req.user.role,
-    });
-});
-
-
-router.post('/users', (req, res) => {
-
-    //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
-    //그것들을  데이터 베이스에 넣어준다.
-
-    const user = new User(req.body);
-
-    user.save((err, doc) => {
-        if(err) return res.json({success: false, err});
-        return res.status(200).json({
-            success: true
-        })
-    })
-
-});
-
-
-router.post("/login", (req, res) => {
-    User.findOne({ id: req.body.id }, (err, user) => {
-        if (!user)
-            return res.json({
-                loginSuccess: false,
-                message: "해당 ID가 존재하지 않습니다."
-            });
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "패스워드가 틀렸습니다." });
-
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("w_authExp", user.tokenExp);
-                res
-                    .cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id, accessToken: user.token
-                    });
-            });
-        });
-    });
-});
-
-router.get("/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true
-        });
-    });
-});
-
-
 // [USER API]
+router.get('/users', function(req, res){
+    User.find({}).then(function(users){
+        res.send(users);
+    });
+});
+
+router.post('/users', function(req, res, next){
+   User.create(req.body).then(function(user){
+    res.send(user);
+   }).catch(next);
+});
+
 
 router.put('/users/:id', function(req, res){
     User.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(user){
@@ -99,9 +42,8 @@ router.get('/events', function(req, res){
     });
 });
 
-const rPostEvents = require(
-    path.join(__dirname, "post-eventImg.js"))
-router.post('/events/post/:id', rPostEvents) //upload image
+const postEventImage = require(path.join(__dirname, "post-eventImg.js"))
+router.post('/events/post/:id', postEventImage) //upload image
 
 
 router.post('/events', function(req, res, next){
