56ec175728f0e7e018b671baa121cad677af455d
olaoot <furin1998@gmail.com>
2021-04-10 18:49:32 +0900

    feat: Link DB

--- src/components/elements/GetEventData.js
+++ src/components/elements/GetEventData.js
@@ -1,6 +1,7 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import event from '../../testData/event.json';
+import axios from 'axios'
 
 
 const ListComponent = styled.div`
@@ -39,35 +40,62 @@ width : ${({width}) => handleWidth(width)};
 font: normal normal 300 20px/29px Spoqa Han Sans Neo;
 `
 
+function returnAxios() {
+  const promise = axios.get()
+  const dataPromise = promise.then((response) => response.data)
+  console.log(dataPromise)
+  return dataPromise
+}
 
-var eventList = event.event;
+const GetEventData = () => {
 
-const GetEventData = () =>
-{
+  const [eventList, setEventList] = useState('vvv')
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getEvents = async () => {
+    const { data: events } = await axios.get("/api/events")
+    console.log(events)
+    setEventList(events)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getEvents()
+	}, [])
 
     return (
-        <ListComponent>
-        <div>
-                {
-                  eventList.map((s) =>{
-                    return (
-                      <Stylediv>
-                        <StyleCB type="checkbox"/>
-                        <StyleTitle width="primary">{s.title}</StyleTitle>
-                        <StyleTitle width="normal">{s.active}</StyleTitle>
-                        <StyleTitle width="middle">{s.Date}</StyleTitle>
+      <div>
+        {isLoading ? 'Loading...' : eventList.map(v => (
+            <>
+              <div>{v.title}</div>
+              <div>{v.available ? '활성화' : '비활성화'}</div>
+              <div>{v.date}~{v.due}</div>
+            </>
+          )
+        )}
+      </div>
+        // <ListComponent>
+        // <div>
+        //         {
+        //           eventList.map((s) =>{
+        //             return (
+        //               <Stylediv>
+        //                 <StyleCB type="checkbox"/>
+        //                 <StyleTitle width="primary">{s.title}</StyleTitle>
+        //                 <StyleTitle width="normal">{s.active}</StyleTitle>
+        //                 <StyleTitle width="middle">{s.Date}</StyleTitle>
                        
                        
-                      </Stylediv>
-                    )
-                  })
+        //               </Stylediv>
+        //             )
+        //           })
 
 
 
-                }
+        //         }
 
-        </div>
-        </ListComponent>
+        // </div>
+        // </ListComponent>
     )
 
 
