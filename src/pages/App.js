43c6d6a3d7f6f94092e410aa27f224360eba7292
boris <boris0815@naver.com>
2021-04-13 14:08:44 +0900

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 import { HashRouter as Router, Route } from "react-router-dom";
 import GlobalStyle from '../styles/GlobalStyle'
 import ClientHomePage from './ClientHomePage'
-
+import ManagerHomePage from './ManagerHomePage'
+import Data from './Data'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ManagerHomePage from './ManagerHomePage';
@@ -24,7 +25,8 @@ const App = () => {
             <Router>
                 <Header />
                 <Route exact path="/" component={ClientHomePage} />
-                <Route exact path="/manager" component={ManagerHomePage}/>
+                <Route exact path="/manager" component={ManagerHomePage} />
+                <Route exact path="/data" component={Data} />
                 <Footer />
             </Router>
         </>
