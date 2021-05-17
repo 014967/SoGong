17d21e763be24e6c7b39b434764020f4d1d809a5
rlawndks4 <ehdanf0303@nate.com>
2021-05-17 14:30:15 +0900

    asdfasdfasdf

--- src/components/SignUpInput.js
+++ src/components/SignUpInput.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from './elements/Button'
 import ContentsWrapper from './elements/ContentsWrapper'
 import styled from 'styled-components'
@@ -16,68 +16,79 @@ const Input = styled.input`
     outline: none;
   }
 `;
+const StyledLogo = styled.div`
+  color: ${props => props.theme.color[props.color] || props.theme.color.primary};
+  font-size: 48px;
+  font-family: ${({ theme }) => theme.font.medium};
+`
 
 const SignUpInput = () => {
 
-const [passwordCheck,setPasswordCheck] = useState('');
-const [passwordError,setPasswordError] = useState(false);
-
-const [id,onChangeId] = useState('');
-const [nick,onChangeNick] = useState('');
-const [password,onChangePassword] = useState('');
+    const [id,setId] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [passwordCheck,setPasswordCheck] = useState('');
+    const [passwordError,setPasswordError] = useState(false);
     
 
-const onSubmit = useCallback((e) => {
-    e.preventDefault();
-    /*검증 로직 만들기
-    비밀번호와 비밀번호 체크가 다를 경우를 검증한다
-     */
-    if(password !== passwordCheck){
-        return setPasswordError(true);
-    }
-    
-},[password,passwordCheck]);    
-    const onChangePasswordChk = useCallback((e) => {
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+       
+         // 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
+         
+        if(password !== passwordCheck){
+            return setPasswordError(true);
+        }
+        
+        console.log({
+            id,
+            email,
+            password,
+            passwordCheck,
+        });
+    };
+
+    // Coustom Hook 이전
+    const onChangeId = (e) => {
+        setId(e.target.value);
+    };
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const onChangePasswordChk = (e) => {
         //비밀번호를 입력할때마다 password 를 검증하는 함수
         setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
-        //password state를 사용하기때문에 password를 넣어준다
-    },[passwordCheck]);
-    
-
-    //반복되는 코드들을 Coustom Hook을 활용하여 줄여줄 수 있다.
-    const useState = (initValue = null) =>{
-        const [value,setter] = useState(initValue);
-        const handler = useCallback((e) => {
-            setter(e.target.value);
-        },[]);
-        return [value,handler];
     };
     
-
     
     return (
         <ContentsWrapper wide>
-            <h1>Sign Up</h1>
+            <StyledLogo>
+            SIGN UP
+            </StyledLogo>
             <form onSubmit={onSubmit}>
             <div>
-            <label htmlFor="user-id">아이디</label><br/>
-            <Input name="user-id" value={id} required onChange={onChangeId} />
+            <label htmlFor="user-id">ID*</label><br/>
+            <Input name="user-id" value={id} placeholder="ID" required onChange={onChangeId} />
             </div>
             <div>
-                <label htmlFor="user-nick">닉네임</label><br/>
-                <Input name="user-nick" value={nick} required onChange={onChangeNick} />
+                <label htmlFor="user-password">PW*</label><br/>
+                <Input name="user-password" type="password" value={password} placeholder="PW" required onChange={onChangePassword} />
             </div>
             <div>
-                <label htmlFor="user-password">비밀번호</label><br/>
-                <Input name="user-password" type="password" value={password} required onChange={onChangePassword} />
-            </div>
-            <div>
-                <label htmlFor="user-password-check">비밀번호체크</label><br/>
-                <Input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordChk} />
+                <label htmlFor="user-password-check">RE-ENTER PW*</label><br/>
+                <Input name="user-password-check" type="password" value={passwordCheck} placeholder="PW" required onChange={onChangePasswordChk} />
                 {passwordError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
             </div>
-            
+            <div>
+                <label htmlFor="user-email">E-MAIL</label><br/>
+                <Input name="user-email" value={email} placeholder="EMAIL@GMAIL.COM" required onChange={onChangeEmail} />
+            </div>
             <div style={{marginTop:10}}>
                 <Button type="primary" htmlType="submit" >가입하기</Button>
             </div>
