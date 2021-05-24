9a0dee44580d693ea276710224ae0fd841b9bb98
olaoot <furin1998@gmail.com>
2021-05-24 22:54:27 +0900

    m

--- src/components/SignUpInput.js
+++ src/components/SignUpInput.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Title from './elements/Title'
 import Button from './elements/Button'
 import ContentsWrapper from './elements/ContentsWrapper'
 import styled from 'styled-components'
@@ -8,7 +9,7 @@ const Input = styled.input`
   width: 300px;
   height: 48px;
   border: solid 1px ${({ theme }) => theme.color.primary};
-  background: ${({ theme }) => theme.color.background};
+  background: white;
   font-size: 20px;
   font-family: ${({ theme }) => theme.font.light};
   padding-left: 8px;
@@ -16,10 +17,14 @@ const Input = styled.input`
     outline: none;
   }
 `;
-const StyledLogo = styled.div`
-  color: ${props => props.theme.color[props.color] || props.theme.color.primary};
-  font-size: 48px;
-  font-family: ${({ theme }) => theme.font.medium};
+
+const Form = styled.form`
+    & > * {
+        margin-top: 32px;
+    }
+    & > *:last-child {
+        margin-bottom: 64px;
+    }
 `
 
 const SignUpInput = () => {
@@ -31,15 +36,12 @@ const SignUpInput = () => {
     const [passwordError,setPasswordError] = useState(false);
     
 
-
     const onSubmit = (e) => {
         e.preventDefault();
        
          // 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
          
-        if(password !== passwordCheck){
-            return setPasswordError(true);
-        }
+        
         
         console.log({
             id,
@@ -48,6 +50,12 @@ const SignUpInput = () => {
             passwordCheck,
         });
     };
+
+    useEffect(() => {
+        if(password !== passwordCheck){
+        return setPasswordError(true);
+    }
+    }, [password, passwordCheck])
 
     // Coustom Hook 이전
     const onChangeId = (e) => {
@@ -67,32 +75,32 @@ const SignUpInput = () => {
     
     
     return (
-        <ContentsWrapper wide>
-            <StyledLogo>
+        <ContentsWrapper>
+            <Title>
             SIGN UP
-            </StyledLogo>
-            <form onSubmit={onSubmit}>
-            <div>
-            <label htmlFor="user-id">ID*</label><br/>
-            <Input name="user-id" value={id} placeholder="ID" required onChange={onChangeId} />
-            </div>
-            <div>
-                <label htmlFor="user-password">PW*</label><br/>
-                <Input name="user-password" type="password" value={password} placeholder="PW" required onChange={onChangePassword} />
-            </div>
-            <div>
-                <label htmlFor="user-password-check">RE-ENTER PW*</label><br/>
-                <Input name="user-password-check" type="password" value={passwordCheck} placeholder="PW" required onChange={onChangePasswordChk} />
-                {passwordError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
-            </div>
-            <div>
-                <label htmlFor="user-email">E-MAIL</label><br/>
-                <Input name="user-email" value={email} placeholder="EMAIL@GMAIL.COM" required onChange={onChangeEmail} />
-            </div>
-            <div style={{marginTop:10}}>
-                <Button type="primary" htmlType="submit" >가입하기</Button>
-            </div>
-        </form>
+            </Title>
+            <Form onSubmit={onSubmit}>
+                <div>
+                <label htmlFor="user-id">ID*</label><br/>
+                <Input name="user-id" value={id} placeholder="ID" required onChange={onChangeId} />
+                </div>
+                <div>
+                    <label htmlFor="user-password">PW*</label><br/>
+                    <Input name="user-password" type="password" value={password} placeholder="PW" required onChange={onChangePassword} />
+                </div>
+                <div>
+                    <label htmlFor="user-password-check">RE-ENTER PW*</label><br/>
+                    <Input name="user-password-check" type="password" value={passwordCheck} placeholder="PW" required onChange={onChangePasswordChk} />
+                    {passwordError && <div style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</div>}
+                </div>
+                <div>
+                    <label htmlFor="user-email">E-MAIL</label><br/>
+                    <Input name="user-email" value={email} placeholder="EMAIL@GMAIL.COM" required onChange={onChangeEmail} />
+                </div>
+                <div>
+                    <Button type="primary" htmlType="submit" >가입하기</Button>
+                </div>
+            </Form>
         </ContentsWrapper>
     );
 }
