0d3e0d2251948da1152d0016dcc28443bee88e99
olaoot <furin1998@gmail.com>
2021-05-25 03:16:46 +0900

    feat: footer

--- src/components/elements/Text.js
+++ src/components/elements/Text.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const Text = styled.div`
   color: ${props => props.theme.color[props.color]};
+  font-size: ${props => props.size};
 `
 
 export default Text
