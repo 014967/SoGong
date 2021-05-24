48ba25018ceca596ce2d6f3288019c13d798d29d
olaoot <furin1998@gmail.com>
2021-05-25 03:59:37 +0900

    feat: mainContents

--- src/components/HomeContents.js
+++ src/components/HomeContents.js
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
+import axios from 'axios'
 import Title from './elements/Title'
 import ContentsWrapper from './elements/ContentsWrapper'
 import HomeContentCards from './HomeContentCards'
 
-const dummyData1 = [
-  {
-    img: '',
-    title: 'Product Sample 1',
-    desc: 'product sample product sample product sample product sample product sample'
-  },
-  {
-    img: '',
-    title: 'Product Sample 2',
-    desc: 'product sample product sample product sample product sample product sample'
-  },
-  {
-    img: '',
-    title: 'Product Sample 3',
-    desc: 'product sample product sample product sample product sample product sample'
-  },
-  {
-    img: '',
-    title: 'Product Sample 4',
-    desc: 'product sample product sample product sample product sample product sample'
-  },
-]
-
-const dummyData2 = [
-  {
-    img: '',
-    title: 'Product Sample 1',
-    desc: 'product sample product sample product sample product sample product sample'
-  },
-  {
-    img: '',
-    title: 'Product Sample 2',
-    desc: 'product sample product sample product sample product sample product sample'
-  }
-]
+// data = {
+//   available: true
+//   category: "남성 상의"
+//   date: "2021-05-23T13:09:30.276Z"
+//   detail: "4"
+//   detailImg: []
+//   detailImgPath: []
+//   img: "1621742970391_1621488599029_1.png"
+//   imgPath: "../assets/images/products/1621742970391_1621488599029_1.png"
+//   name: "0523130930"
+//   price: 4
+//   productNo: 0
+//   stock: 4
+//   token: false
+//   __v: 0
+//   _id: "60a9d57aeade87699b3cc861"
+// }
 
 const HomeContents = () => {
-    return (
-        <ContentsWrapper>
-          <Title>BEST</Title>
-          <HomeContentCards data={dummyData1} />
-          <Title>NEW</Title>
-          <HomeContentCards data={dummyData2} />
-        </ContentsWrapper>
-    );
+
+  const [data, setData] = useState([])
+
+  const getNewProduct = async () => {
+    const { data: products } = await axios.get("/api/products/")
+    .catch(err => console.log(err))
+    setData(products.slice(0, 4))
+    console.log(products.slice(0, 4))
+  }
+
+  useEffect(() => {
+    getNewProduct()
+  }, [])
+
+  return (
+    <ContentsWrapper>
+      <Title>NEW</Title>
+      <HomeContentCards data={data} />
+    </ContentsWrapper>
+  );
 }
 
 export default HomeContents
