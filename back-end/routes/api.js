0921cd0de1e15d5954db2231fcb53fb34a64dfd1
Jun <rozo1136@gmail.com>
2021-05-27 03:46:58 +0900

    paging issue

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -641,12 +641,15 @@ router.post('/products/sortedPage', function(req, res){
     let page = req.body.page
     if(page == 1) Product.find({'name': {'$regex': req.body.search,'$options': 'i' },
     price:{"$gte":req.body.min,"$lte":req.body.max}, available : true}).sort({price: req.body.order})
+    .limit(10)
    .then(function(product){
         res.send(product);
     });
 
     if(page!=1) Product.find({'name': {'$regex': req.body.search,'$options': 'i' },
     price:{"$gte":req.body.min,"$lte":req.body.max}, available : true}).sort({price: req.body.order})
+    .skip( (page-1) * 10 )
+    .limit(10)
    .then(function(product){
         res.send(product);
     });
@@ -691,16 +694,19 @@ router.post('/products/unsorted', function(req, res){
 });
 
 router.post('/products/unsortedPage', function(req, res){
-
-
+    
+    
     if(req.body.page == 1) Product.find({'name': {'$regex': req.body.search,'$options': 'i' },
     price:{"$gte":req.body.min,"$lte":req.body.max}, available : true}).sort({date: -1})
+    .limit(10)
    .then(function(product){
         res.send(product)
     });
 
     if(req.body.page!=1) Product.find({'name': {'$regex': req.body.search,'$options': 'i' },
     price:{"$gte":req.body.min,"$lte":req.body.max}, available : true}).sort({date: -1})
+    .skip( (req.body.page-1) * 10 )
+    .limit(10)
    .then(function(product){
         res.send(product)
 
