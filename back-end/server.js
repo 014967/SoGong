b4aa5641bc7b7768e60a4d82f08fd67cd7c4f7f1
Jun <rozo1136@gmail.com>
2021-05-27 15:06:21 +0900

    delete dotenv

--- back-end/server.js
+++ back-end/server.js
@@ -184,7 +184,7 @@ const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
-
+console.log(process.env)
 /*
 const { AWS_config_region, AWS_IDENTITYPOOLID } = process.env
 const bucket = "sogong17"
