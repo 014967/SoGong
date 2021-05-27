439735524cc16e5562d24d58d84495d64c605da6
EC2 Default User <ec2-user@ip-172-31-35-61.ap-northeast-2.compute.internal>
2021-05-27 16:40:33 +0000

    master

--- back-end/server.js
+++ back-end/server.js
@@ -43,149 +43,32 @@ mongoose.connect('mongodb+srv://Junhyong:Junhyong@cluster0.bm9aa.mongodb.net/Tea
 mongoose.Promise = global.Promise;
 
 // [GRIDFS STORAGE ENGINE]
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream')
-const crypto = require('crypto');
+
+
+
 const multer = require('multer');
 const path = require('path');
 
- let gfs;
- conn.once('open', () => {
-    // Init stream
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-  });
-
-  // Create storage engine
-const storage = new GridFsStorage({
-    url: mongoURI,
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        crypto.randomBytes(16, (err, buf) => {
-          if (err) {
-            return reject(err);
-          }
-          const filename = buf.toString('hex') + path.extname(file.originalname);
-          const fileInfo = {
-            filename: filename,
-            bucketName: 'uploads'
-          };
-          resolve(fileInfo);
-        });
-      });
-    }
-  });
-
-  const upload = multer({ storage });
-
- 
-app.post('/upload', upload.single('file'), (req, res) => {
-    // res.json({ file: req.file });
-    res.redirect('/');
-  });
-  
-  
-  app.get('/upload', (req, res) => {
-    gfs.files.find().toArray((err, files) => {
-      // Check if files
-      if (!files || files.length === 0) {
-        return res.status(404).json({
-          err: 'No files exist'
-        });
-      }
-  
-      // Files exist
-      return res.json(files);
-    });
-  });
-
- 
-app.get('/upload/:filename', (req, res) => {
-    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-      // Check if file
-      if (!file || file.length === 0) {
-        return res.status(404).json({
-          err: 'No file exists'
-        });
-      }
-      // File exists
-      return res.json(file);
-    });
-  });
-
-  app.get('/upload64/:filename', (req, res) => {
-    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-      // Check if file
-      if (!file || file.length === 0) {
-        return res.status(404).json({
-          err: 'No file exists'
-        });
-      }
-      // File exists
-      const readstream = gfs.createReadStream(file.filename);
-      
-      const bufs = [];
-      readstream.on('data', function (chunk) {
-        bufs.push(chunk);
-      });
-      readstream.on('end', function () {
-        const fbuf = Buffer.concat(bufs);
-        const base64 = fbuf.toString('base64');
-      });
-      return res.json(file);
-    });
-  });
 
 
-  
-  
-  app.get('/upload/display/:filename', (req, res) => {
-    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-      // Check if file
-      if (!file || file.length === 0) {
-        return res.status(404).json({
-          err: 'No file exists'
-        });
-      }
-  
-      // Check if image
-      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-        // Read output to browser
-        const readstream = gfs.createReadStream(file.filename);
-        readstream.pipe(res);
-      } else {
-        res.status(404).json({
-          err: 'Not an image'
-        });
-      }
-    });
-  });
-  
-  
-  app.delete('/upload/:id', (req, res) => {
-    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
-      if (err) {
-        return res.status(404).json({ err: err });
-      }
-  
-      res.redirect('/');
-    });
-  });
 
 // [SERVERSIDE UPLOAD WITH MULTER]
 const Event = require('./models/events');
 const Product = require('./models/products');
 const AWS = require('aws-sdk');
 var multerS3 = require("multer-s3");
-
-
 const dotenv = require('dotenv') 
 dotenv.config()
 
-const { AWS_config_region, AWS_IDENTITYPOOLID, AWSAccessKeyId, AWSSecretKey } = process.env
-console.log(AWS_IDENTITYPOOLID)
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+console.log(process.env)
+/*
+const { AWS_config_region, AWS_IDENTITYPOOLID } = process.env
 const bucket = "sogong17"
-
+console.log(bucket)
 AWS.config.update({
   region : AWS_config_region,
   credentials : new AWS.CognitoIdentityCredentials({
@@ -195,15 +78,7 @@ AWS.config.update({
 
 const s3 = new AWS.S3({
   apiVersion: "2006-03-01",
-  params: { 
-    Bucket: bucket,
-    accessKeyId: AWSAccessKeyId,
-    secretAccessKey: AWSSecretKey,
-    region: AWS_config_region,
-  },
-  accessKeyId: AWSAccessKeyId,
-  secretAccessKey: AWSSecretKey,
-  region: AWS_config_region,
+  params: {Bucket: bucket}
 });
 
 const uploadS3Product = multer({
@@ -231,13 +106,18 @@ const uploadS3Event = multer({
     }
   }),
 });
-/*const multerS3 = require('multer-s3');
+*/
+/*
+const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
 require('dotenv').config()
+const S3 = require('aws-sdk/clients/s3')
+*/
+
 const S3 = require('aws-sdk/clients/s3')
 
 const s3 = new S3({
@@ -252,7 +132,7 @@ const s3 = new S3({
      bucket: bucketName,
      acl: 'public-read',
      key: function(req, file, cb) {
-      cb(null, '../src/assets/images/products/'+new Date().valueOf() + '_'+file.originalname)
+      cb(null, 'products/'+new Date().valueOf() + '_'+file.originalname)
      }
    })
   })
@@ -263,39 +143,13 @@ const s3 = new S3({
       bucket: bucketName,
       acl: 'public-read',
       key: function(req, file, cb) {
-       cb(null, '../src/assets/images/banners/'+new Date().valueOf() + '_'+file.originalname)
+       cb(null, 'banners/'+new Date().valueOf() + '_'+file.originalname)
       }
     })
    })
-*/
-/////////////////////////////
 
-  // const mupload = multer({
-  //   storage: multer.diskStorage({
-  //     destination: function (req, file, cb) {
-  //       cb(null, '../src/assets/images/banners/');
-  //     },
-  //     filename: function (req, file, cb) {
-  //       cb(null, new Date().valueOf() + '_' + file.originalname);
-  //     }
-  //   }),
-  // });
-
-  // const pupload = multer({
-  //   storage: multer.diskStorage({
-  //     destination: function (req, file, cb) {
-  //       cb(null, '../src/assets/images/products/');
-  //     },
-  //     filename: function (req, file, cb) {
-  //       cb(null, new Date().valueOf() + '_' + file.originalname);
-  //     }
-  //   }),
-  // });
 
 app.post('/eventImg/:id', uploadS3Event.single('img'), (req, res) => {
-  console.log(req.file)
-  console.log(req)
-  console.log(req.file.location ? 'zz' : 'z')
   Event.findByIdAndUpdate(
     {_id: req.params.id}, {img: req.file.location, imgPath: req.file.location}).then(function(event){
 
@@ -305,16 +159,16 @@ app.post('/eventImg/:id', uploadS3Event.single('img'), (req, res) => {
   })
 });
 
-// app.post('/productImg/:id', uploadS3Event.single('img'), (req, res) => {
-//   Product.findByIdAndUpdate(
-//     {_id: req.params.id}, {img: req.file.location, imgPath: req.file.location}).then(function(product){
+app.post('/productImg/:id', uploadS3Event.single('img'), (req, res) => {
+  Product.findByIdAndUpdate(
+    {_id: req.params.id}, {img: req.file.location, imgPath: req.file.location}).then(function(product){
 
-//     Product.findOne({_id: req.params.id}).then(function(product){
-//       console.log('successfully updated local image');
-//       res.send('Success');
-//     })
-//   })
-// });
+    Product.findOne({_id: req.params.id}).then(function(product){
+      console.log('successfully updated local image');
+      res.send('Success');
+    })
+  })
+});
 
 // mutiple product images upload below.
 app.post('/productMutipleImg/:id', uploadS3Product.array('img', 5), (req, res) => {
