04f082b7076fb5786276e891444183d95dfcf011
olaoot <furin1998@gmail.com>
2021-06-01 04:05:48 +0900

    fix(schema)

--- back-end/models/customerservice.js
+++ back-end/models/customerservice.js
@@ -18,12 +18,21 @@ const CustomerserviceSchema = mongoose.Schema({
         default: ""
     },
     orderlist: {
-        type: String,
-        required : [true]
+        type: String
     },
     date: {
         type: Date,
-        default: Date.now()
+        default: k_date = () => {
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = date.getMonth();
+            let today = date.getDate();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+            let milliseconds = date.getMilliseconds();
+            return new Date(Date.UTC(year, month, today, hours, minutes, seconds, milliseconds));
+        }
     },
 })
 
--- back-end/models/review.js
+++ back-end/models/review.js
@@ -28,7 +28,17 @@ const ReviewSchema = mongoose.Schema({
     },
     date: {
         type: Date,
-        default: Date.now()
+        default: k_date = () => {
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = date.getMonth();
+            let today = date.getDate();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+            let milliseconds = date.getMilliseconds();
+            return new Date(Date.UTC(year, month, today, hours, minutes, seconds, milliseconds));
+        }
     },
 })
 
